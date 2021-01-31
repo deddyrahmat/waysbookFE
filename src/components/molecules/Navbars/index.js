e98235580dc6027efc793b99b959b66813569bcf
deddyrahmat <deddyrahmat97@gmail.com>
2021-01-31 18:09:17 +0700

    add transaction

--- src/components/molecules/Navbars/index.js
+++ src/components/molecules/Navbars/index.js
@@ -32,7 +32,7 @@ const Navbars = () => {
         <Fragment>
             <Navbar className="mt-3">
                 <Container>                    
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand as={Link} to="/admin/dashboard">
                         <img src={LogoSidebar} alt="Logo" className="img-fluid" />
                     </Navbar.Brand>
                     <Navbar.Toggle />
--- src/components/molecules/Transactions/index.js
+++ src/components/molecules/Transactions/index.js
@@ -1,22 +1,23 @@
 import React, { Fragment } from 'react'
 import { Table, NavDropdown } from 'react-bootstrap'
-import {Link} from 'react-router-dom';
+
+import "./Transactions.css";
 
 const Transactions = () => {
     return (
         <Fragment>
-            <h3 className="title-dashboard">Income Transaction</h3>
+            <h3 className="title-dashboard">Incoming Transaction</h3>
             
-            <Table striped hover variant="light">
+            <Table striped hover variant="light" id="table-transaction">
                     <thead className="text-danger">
                         <tr>
-                        <th>No</th>
-                        <th>Users</th>
-                        <th>Bukti Transfer</th>
-                        <th>Remaining Active</th>
-                        <th>Status User</th>
-                        <th>Status Payment</th>
-                        <th>Action</th>
+                        <th className="head-transactions">No</th>
+                        <th className="head-transactions">Users</th>
+                        <th className="head-transactions">Bukti Transfer</th>
+                        <th className="head-transactions">Remaining Active</th>
+                        <th className="head-transactions">Status User</th>
+                        <th className="head-transactions">Status Payment</th>
+                        <th className="head-transactions">Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -26,11 +27,11 @@ const Transactions = () => {
                             <td>bca.jpg</td>
                             <td>26 / Hari</td>
                             <td className="text-success">Active</td>
-                            <td className="text-success">Approve</td>
+                            <td className="status-payment-approve">Approve</td>
                             <td>
                                 <NavDropdown id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.3" className="text-success">Approved</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.4" className="text-danger">Cancel</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3" className="status-payment-approve">Approved</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.4" className="status-payment-cancel">Cancel</NavDropdown.Item>
                                 </NavDropdown>
                             </td>
                         </tr>
@@ -40,11 +41,11 @@ const Transactions = () => {
                             <td>bni.jpg</td>
                             <td>0 / Hari</td>
                             <td className="text-danger">Not Active</td>
-                            <td className="text-danger">Cancel</td>
+                            <td className="status-payment-cancel">Cancel</td>
                             <td>
                                 <NavDropdown id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.3" className="text-success">Approved</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.4" className="text-danger">Cancel</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3" className="status-payment-approve">Approved</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.4" className="status-payment-cancel">Cancel</NavDropdown.Item>
                                 </NavDropdown>
                             </td>
                         </tr>
@@ -54,11 +55,11 @@ const Transactions = () => {
                             <td>bri.jpg</td>
                             <td>0 / Hari</td>
                             <td className="text-danger">Not Active</td>
-                            <td className="text-warning">Pending</td>
+                            <td className="status-payment-pending">Pending</td>
                             <td>
                                 <NavDropdown id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.3" className="text-success">Approved</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.4" className="text-danger">Cancel</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3" className="status-payment-approve">Approved</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.4" className="status-payment-cancel">Cancel</NavDropdown.Item>
                                 </NavDropdown>
                             </td>
                         </tr>
