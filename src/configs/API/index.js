6f13cb7063ac4b1fc6fda5e7928588df8dc85672
deddyrahmat <deddyrahmat97@gmail.com>
2021-03-01 08:55:07 +0700

    prepare deploy to netlify

--- src/configs/API/index.js
+++ src/configs/API/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    baseURL : "http://localhost:5000/api/v1"
-    // baseURL : "https://window-of-world.herokuapp.com/api/v1"
+    // baseURL : "http://localhost:5000/api/v1"
+    baseURL : "https://waysbook.herokuapp.com/api/v1"
 });
 
 export const setAuthToken = (token) => {
